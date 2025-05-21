555c58472bb20878ff1cea2022d5910c04b9ea46
Joaquín Jiménez Cedrón <142637048+joajced@users.noreply.github.com>
2025-05-21 21:24:45 +0200

    Add main.c
    Code is still empty

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+#include "init.h"
+#include "delay.h"
+#include "LCD_GUI.h"
+#include "LCD_Demos.h"
+#include "lcd.h"
+#include "fontsFLASH.h"
+#include "LCD_Touch.h"
+#include "error.h"
+
+int main()
+{
+	initITSboard();
+	GUI_init(DEFAULT_BRIGHTNESS);
+	
+	// Code goes here
+}
