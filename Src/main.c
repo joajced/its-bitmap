3f537a34716e2971a644e8dab2cdfed78db1f3c9
Joaquín Jiménez Cedrón <142637048+joajced@users.noreply.github.com>
2025-05-21 22:53:58 +0200

    Add general program structure

--- Src/main.c
+++ Src/main.c
@@ -2,7 +2,13 @@
 #include "init.h"
 #include "LCD_GUI.h"
 #include "BMP_types.h"
-#include "delay.h" // TESTEN
+#include "input.h"
+#include "button_input.h"
+
+// ZUM TESTEN
+#include "lcd.h"
+#include "delay.h"
+#include <stdio.h>
 
 #define DISPLAY_LENGTH 480
 #define DISPLAY_HEIGHT 320
@@ -11,15 +17,29 @@ int main()
 {
 	initITSboard();
 	GUI_init(DEFAULT_BRIGHTNESS);
+	initInput();
 	
-	for (WORD y = 0; y < DISPLAY_HEIGHT; y++)
+	while (1)
 	{
-		for (WORD x = 0; x < DISPLAY_LENGTH; x++)
+		// 1. Wait for file (already done inside function)
+		
+		openNextFile();
+		
+		// 2. Process file and print to display
+		
+		// COMread und/oder nextChar
+		
+		for (WORD y = 0; y < DISPLAY_HEIGHT; y++)
 		{
-			Coordinate temp = {x, y};
-			GUI_drawPoint(temp, GREEN, DOT_PIXEL_1X1, DOT_FILL_AROUND);
+			for (WORD x = 0; x < DISPLAY_LENGTH; x++)
+			{
+				Coordinate temp = {x, y};
+				GUI_drawPoint(temp, GREEN, DOT_PIXEL_1X1, DOT_FILL_AROUND);
+			}
 		}
+		
+		// 3. Wait for button for next file
+		
+		while (!isButtonPressed(S7));
 	}
-	
-	while (1);
 }
