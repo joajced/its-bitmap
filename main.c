1ea6230d4b41d0b0f6173e15810b93222b32a551
Joaquín Jiménez Cedrón <142637048+joajced@users.noreply.github.com>
2025-05-21 21:58:06 +0200

    Gerüst fürs Programm

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include "init.h"
-#include "delay.h"
 #include "LCD_GUI.h"
-#include "LCD_Demos.h"
-#include "lcd.h"
-#include "fontsFLASH.h"
-#include "LCD_Touch.h"
-#include "error.h"
+#include "BMP_types.h"
+#include "delay.h" // TESTEN
+
+#define DISPLAY_LENGTH 480
+#define DISPLAY_HEIGHT 320
 
 int main()
 {
 	initITSboard();
 	GUI_init(DEFAULT_BRIGHTNESS);
 	
-	// Code goes here
+	for (WORD y = 0; y < DISPLAY_HEIGHT; y++)
+	{
+		for (WORD x = 0; x < DISPLAY_LENGTH; x++)
+		{
+			Coordinate temp = {x, y};
+			GUI_drawPoint(temp, GREEN, DOT_PIXEL_1X1, DOT_FILL_AROUND);
+		}
+	}
+	
+	while (1);
 }
